fff00254d101619534a5e35d760be718192442c2
Prateek1337 <prateeksahu1337@gmail.com>
2021-02-27 19:46:31 +0530

    added model in html

--- credible/credible/credible_app/views.py
+++ credible/credible/credible_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 import os
 import requests
+from .models import WebsiteInfo
 
 # Create your views here.
 def index(request):
@@ -9,15 +10,24 @@ def index(request):
 
 def reviewsite(request):
     search_key = request.POST['search']
+    url='https://www.'+search_key+'.com'
+    print(search_key)
     try:
-        response = requests.get(search_key)
+        response = requests.get(url)
         if response.status_code == 200:
             print('Web site exists')
         else:
             print('Web site does not exist') 
     except:
         print("No response")
-    return render(request, 'reviewsite.html')
+        return HttpResponse("No response from website")
+    
+    print(WebsiteInfo.objects.get(pk=search_key))
+    try:
+        website=WebsiteInfo.objects.get(pk=search_key)
+        return render(request, 'reviewsite.html',{'website': website})
+    except WebsiteInfo.DoesNotExist:
+        return HttpResponse("Website does not exist  in database")
 
 def signup(request):
     return render(request,'signup.html')
