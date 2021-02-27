b17babeb9b5159d97e56b9d72935d8b26789d440
Prateek1337 <prateeksahu1337@gmail.com>
2021-02-27 22:12:31 +0530

    added submit button

--- credible/credible/credible_app/models.py
+++ credible/credible/credible_app/models.py
@@ -5,25 +5,31 @@ class User(models.Model):
     email = models.CharField(max_length=200,primary_key=True)
     name = models.CharField(max_length=200)
     number_of_rating=models.IntegerField(default=0)
-    weight=models.IntegerField(default=1)
+    weight=models.FloatField(default=1)
+
 
 
 class WebsiteInfo(models.Model):
     name=models.CharField(max_length=200,primary_key=True)
     url=models.CharField(max_length=200)
-    twitter=models.CharField(max_length=200)
-    wiki=models.CharField(max_length=200)
+    twitter=models.CharField(max_length=200,default='sample@twitter')
+    wiki=models.CharField(max_length=200,default='sample@Wiki')
+    location=models.CharField(max_length=200,default='sample@Headquaters')
     average=models.FloatField(default=0.0)
     average_media=models.FloatField(default=0.0)
     average_entertainment=models.FloatField(default=0.0)
     average_fact=models.FloatField(default=0.0)
+    number_entertainment_reviews=models.IntegerField(default=0)
+    number_media_reviews=models.IntegerField(default=0)
+    number_fact_reviews=models.IntegerField(default=0)
+
 
 
 class Review(models.Model):
     rating_choices = [
         ('News', 'News'),
         ('Entertainment', 'Entertainment'),
-        ('Facts', 'Facts'),
+        ('Fact', 'Fact'),
         ('Overall', 'Overall'),
     ]
     email=models.ForeignKey(User , on_delete=models.CASCADE)
--- credible/credible/credible_app/views.py
+++ credible/credible/credible_app/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse,Http404
 import os
 import requests
-from .models import WebsiteInfo, User
+from .models import WebsiteInfo, User, Review
 from django.shortcuts import render,redirect
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+user_email="test"
 def index(request):
+    print(user_email)
     return render(request, 'index.html')
 
 def reviewsite(request):
+    global user_email
+    print(user_email)
+    print(request.POST)
     search_key = request.POST['search']
     url='https://www.'+search_key+'.com'
     print(search_key)
@@ -23,7 +28,7 @@ def reviewsite(request):
         print("No response")
         return HttpResponse("No response from website")
     
-    print(WebsiteInfo.objects.get(pk=search_key))
+    # print(WebsiteInfo.objects.get(pk=search_key))
     try:
         website=WebsiteInfo.objects.get(pk=search_key)
         return render(request, 'reviewsite.html',{'website': website})
@@ -52,8 +57,43 @@ def login(request):
         try: 
             print(request.POST)
             User.objects.get(email = request.POST["email"])
+            global user_email
+            user_email=request.POST['email']
             return render(request,'index.html')
         except:
             print("ok")
             return render(request, 'login.html')
     return render(request, 'login.html')
+
+
+
+def updateCredibility(website,review):
+    user=User.objects.get(pk = user_email)
+    reviews=Review.objects.query(category=review.category)
+    weight_sum=0
+    for r in reviews:
+        temp_user=User.objects.get(pk=r.email)
+        weight_sum=weight_sum+temp_user.weight
+
+    if(review.category=='News'):
+        print('News Updated')
+        new_media_average=(website.average_media*(weight_sum-user.weight)+user.weight*review.rating)/(weight_sum)
+        website.number_media_reviews=website.number_media_reviews+1
+        website.average_media=new_media_average
+    elif(review.category=='Entertainment'):
+        print('ENT Updated')
+        new_entertainment_average=(website.average_entertainment*website.number_entertainment_reviews+user.weight*review.rating)/(website.number_entertainment_reviews+1)
+        website.number_entertainment_reviews=website.number_entertainment_reviews+1
+        website.average_entertainment=new_entertainment_average
+    
+    elif(review.category=='Fact'):
+        print('fact Updated')
+        new_fact_average=(website.average_fact*website.number_fact_reviews+user.weight*review.rating)/(website.number_fact_reviews+1)
+        website.number_fact_reviews=website.number_fact_reviews+1
+        website.average_fact=new_fact_average
+    
+    website.average=(website.average_entertainment+website.average_fact+website.average_media)/3
+    website.save()
+    
+
+
